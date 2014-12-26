8010104534f19de531d95d85c214ab679a7286ec
10sr <8slashes+git@gmail.com>
2014-12-26 11:56:46 +0900

    Define macro to check result

--- test/shell-split-string.el
+++ test/shell-split-string.el
@@ -1,41 +1,47 @@
 (require 'ert)
 (require 'shell-split-string)
 
+(defmacro ert-sss-should-equal (str result)
+  "Judge if `shell-split-string' returns expected RESULT from STR."
+  `(should (equal (shell-split-string ,str)
+                  ,result))
+  )
+
 (ert-deftest test-shell-split-string ()
-  (should (equal (shell-split-string "abc")
-                 '("abc")))
-  (should (equal (shell-split-string "abc def")
-                 '("abc" "def")))
-  (should (equal (shell-split-string " abc def ")
-                 '("abc" "def")))
-  (should (equal (shell-split-string "abc  def")
-                 '("abc" "def")))
-  (should (equal (shell-split-string "abc \"def\"")
-                 '("abc" "def")))
-  (should (equal (shell-split-string "abc 'def'")
-                 '("abc" "def")))
-  (should (equal (shell-split-string "abc  \"def\"")
-                 '("abc" "def")))
-  (should (equal (shell-split-string "abc \"def  ghi\"")
-                 '("abc" "def  ghi")))
-  (should (equal (shell-split-string "abc 'def  ghi'")
-                 '("abc" "def  ghi")))
-  (should (equal (shell-split-string "abc \"d\\\"ef\"")
-                 '("abc" "d\"ef")))
-  (should (equal (shell-split-string "abc \"d'ef\"")
-                 '("abc" "d'ef")))
-  (should (equal (shell-split-string "abc 'de f'ghi")
-                 '("abc" "de fghi")))
-  (should (equal (shell-split-string "abc def'gh i'")
-                 '("abc" "defgh i")))
-  (should (equal (shell-split-string "abc de'fg  h'i")
-                 '("abc" "defg  hi")))
-  (should (equal (shell-split-string "a\\bc")
-                 '("abc")))
-  (should (equal (shell-split-string "\"a\\bc\"")
-                 '("a\\bc")))
-  (should (equal (shell-split-string "'a\\bc'")
-                 '("a\\bc")))
-  (should (equal (shell-split-string "abc && def")
-                 '("abc" "&&" "def")))
+  (ert-sss-should-equal "abc"
+                        '("abc"))
+  (ert-sss-should-equal "abc def"
+                        '("abc" "def"))
+  (ert-sss-should-equal " abc def "
+                        '("abc" "def"))
+  (ert-sss-should-equal "abc  def"
+                        '("abc" "def"))
+  (ert-sss-should-equal "abc \"def\""
+                        '("abc" "def"))
+  (ert-sss-should-equal "abc 'def'"
+                        '("abc" "def"))
+  (ert-sss-should-equal "abc  \"def\""
+                        '("abc" "def"))
+  (ert-sss-should-equal "abc \"def  ghi\""
+                        '("abc" "def  ghi"))
+  (ert-sss-should-equal "abc 'def  ghi'"
+                        '("abc" "def  ghi"))
+  (ert-sss-should-equal "abc \"d\\\"ef\""
+                        '("abc" "d\"ef"))
+  (ert-sss-should-equal "abc \"d'ef\""
+                        '("abc" "d'ef"))
+  (ert-sss-should-equal "abc 'de f'ghi"
+                        '("abc" "de fghi"))
+  (ert-sss-should-equal "abc def'gh i'"
+                        '("abc" "defgh i"))
+  (ert-sss-should-equal "abc de'fg  h'i"
+                        '("abc" "defg  hi"))
+  (ert-sss-should-equal "a\\bc"
+                        '("abc"))
+  (ert-sss-should-equal "\"a\\bc\""
+                        '("a\\bc"))
+  (ert-sss-should-equal "'a\\bc'"
+                        '("a\\bc"))
+  (ert-sss-should-equal "abc && def"
+                        '("abc" "&&" "def"))
   )
