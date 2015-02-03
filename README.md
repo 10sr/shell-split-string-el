062b85f333a0a60ae09207530544a95d9b001581
10sr <8slashes+git@gmail.com>
2015-02-03 13:36:07 +0900

    Add some a little complex examples

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ and it works like:
 (shell-split-string "abc")  ->  '("abc")
 (shell-split-string "abc  def")  ->  '("abc" "def")
 (shell-split-string "abc \"def ghi\"")  ->  '("abc" "def ghi")
+(shell-split-string "abc 'de f'ghi") -> '("abc" "de fghi")
+(shell-split-string "abc \"d\\\"ef\"") -> '("abc" "d\"ef")
 ```
 
 
--- shell-split-string.el
+++ shell-split-string.el
@@ -40,6 +40,8 @@
 ;; (shell-split-string "abc")  ->  '("abc")
 ;; (shell-split-string "abc  def")  ->  '("abc" "def")
 ;; (shell-split-string "abc \"def ghi\"")  ->  '("abc" "def ghi")
+;; (shell-split-string "abc 'de f'ghi") -> '("abc" "de fghi")
+;; (shell-split-string "abc \"d\\\"ef\"") -> '("abc" "d\"ef")
 
 
 ;;; Code:
