f2b538a34abc68d58600907dbccd8ec9756f94a7
10sr <8slashes+git@gmail.com>
2015-02-05 11:18:49 +0900

    Print what tests are conducted

--- test/shell-split-string.el
+++ test/shell-split-string.el
@@ -2,18 +2,26 @@
 
 (defmacro ert-sss-should-equal (str result)
   "Judge if `shell-split-string' returns expected RESULT from STR."
-  `(should (equal (shell-split-string ,str)
-                  ,result))
+  `(progn
+     (message "Should eq    : %S\t->\t%S"
+              ,str ,result)
+     (should (equal (shell-split-string ,str)
+                  ,result)))
   )
 
 (defmacro ert-sss-should-error (str)
   "Judge if `shell-split-string' throws error with arg STR."
-  `(should-error (shell-split-string ,str)))
+  `(progn
+     (message "Should err   : %S"
+              ,str)
+     (should-error (shell-split-string ,str))))
 
 (defmacro ert-sss-inverse-check (args)
   "Check if `shell-split-string' is an inverse function of `mapconcat' and
  `shell-quote-argument' by ARGS."
   `(let ((a ,args))
+     (message "Inverse check: %S"
+              a)
      (should (equal (shell-split-string (mapconcat 'shell-quote-argument
                                                    a
                                                    " "))
